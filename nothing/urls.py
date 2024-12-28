e4e38ecdb7911f9c01361e7f6f6bc5088a88fdc8
Soliha1607 <solihapahridinova@gamil.com>
2024-12-28 15:28:59 +0500

    first commit

--- nothing/urls.py
+++ nothing/urls.py
@@ -18,13 +18,13 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('organvolarman.urls')),
-]
+    re_path(r"static/(?P<path>.*)", serve,  {"document_root": settings.STATIC_ROOT}),
+    re_path(r"media/(?P<path>.*)", serve,  {"document_root": settings.MEDIA_ROOT})
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
